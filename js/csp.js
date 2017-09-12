923deb7994f5bc12bf28a6d7aac73b3499088742
CaSipy <659551976@qq.com>
2017-09-12 10:29:05 +0800

    修改

---
+++ js/csp.js
@@ -0,0 +1,7 @@
+$(".keySkco1").css('width',173);
+$(".keySkco2").css('width',173);
+$(".keySkco3").css('width',213);
+$(".keySkco4").css('width',163);
+$(".keySkco5").css('width',193);
+$(".keySkco6").css('width',153);
+$(".mydream").css("opacity",1)
