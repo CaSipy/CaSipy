d2d9bab06a4f5aab439b9699d6435096703aa139
CaSipy <659551976@qq.com>
2017-09-12 18:00:37 +0800

    系

--- js/csp.js
+++ js/csp.js
@@ -4,4 +4,10 @@ $(".keySkco3").css('width',213);
 $(".keySkco4").css('width',163);
 $(".keySkco5").css('width',193);
 $(".keySkco6").css('width',153);
-$(".mydream").css("opacity",1)
+$(".mydream").css("opacity",1);
+//var d=document.body.scrollHeight;
+//$(window).scroll(function() {
+//	if ($(this).scrollTop() >= d*.1 &&$(this).scrollTop()<d*.3) {
+//		console.log('1');
+//	}
+//});
