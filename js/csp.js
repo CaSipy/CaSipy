67241c60972130d6c98b58eefec89ede3fd4a15a
CaSipy <659551976@qq.com>
2017-09-13 21:46:20 +0800

    增加简历页面

---
+++ js/csp.js
@@ -0,0 +1,13 @@
+$(".keySkco1").css('width',173);
+$(".keySkco2").css('width',173);
+$(".keySkco3").css('width',213);
+$(".keySkco4").css('width',163);
+$(".keySkco5").css('width',193);
+$(".keySkco6").css('width',153);
+$(".mydream").css("opacity",1);
+//var d=document.body.scrollHeight;
+//$(window).scroll(function() {
+//	if ($(this).scrollTop() >= d*.1 &&$(this).scrollTop()<d*.3) {
+//		console.log('1');
+//	}
+//});
